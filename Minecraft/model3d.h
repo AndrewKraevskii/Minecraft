3c47a2b1c765e7e24472ccd731f55208577ebb5e
AndrewKraevskii <andrew.kraevskii@gmail.com>
2021-05-03 14:15:02 +0300

    Добавлена загрузка текстуры из файла png и установлено ограничение на 60 fps

---
+++ Minecraft/model3d.h
@@ -0,0 +1,25 @@
+#pragma once
+
+#include <fstream>
+#include <string>
+#include <vector>
+
+#include "vectors.h"
+
+class Model3d {
+  uint32_t texture[1];
+  std::vector<Vector3d> vertexes;
+  std::vector<Vector2d> UVs;
+  std::vector<Vector3d> normals;
+  std::vector<std::vector<Vector3i>> faces;
+
+  void loadOBJ(std::string file_name);
+
+ public:
+  Model3d(std::string file_name);
+  Model3d(std::string file_obj, std::string texture_file);
+
+  void draw();
+
+  ~Model3d();
+};
